d1b98e0e872de9bbf5c3f8c06496035012e8abe3
Sonny Ferdinand <90435664+SiGebeelek@users.noreply.github.com>
2025-12-05 16:28:18 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import joblib
 import streamlit as st
 
 st.set_page_config(page_title="Business Finance Analytics", layout="centered")
